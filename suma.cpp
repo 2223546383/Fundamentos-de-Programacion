2980d3dd6bc70877cf9512f86741f4d64787e72a
2223546383 <uriel_drack@hotmail.com>
2015-11-02 23:39:45 -0600

    Programa que suma 2 numero

---
+++ suma.cpp
@@ -0,0 +1,12 @@
+//Programa que suma 2 numero 
+#include<stdio.h>
+int numero1;
+int numero2;
+int resultado;
+int main()
+{
+	numero1=10;
+	numero2=20;
+	resultado= numero1+numero2;
+	printf("El resultado es %d", resultado);				
+}
